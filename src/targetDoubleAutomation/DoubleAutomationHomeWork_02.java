e8532933d345985c2438b2f22776d5abf7fb0150
NadiaEti <nadiaeti67@gmail.com>
2022-07-05 18:02:48 -0400

    Double Automation Practice update

--- src/targetDoubleAutomation/DoubleAutomationHomeWork_02.java
+++ src/targetDoubleAutomation/DoubleAutomationHomeWork_02.java
@@ -1,9 +1,11 @@
 package targetDoubleAutomation;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -11,37 +13,53 @@ public class DoubleAutomationHomeWork_02 {
 
 //Test Base class: Configuration
 
-//
-//        public static WebDriver driver;
-//        String browserName = "chrome";
-//        static String url = "https://www.target.com/";
-//
-//
-//        @BeforeTest
-//        public void setUpAutomation() {
-//
-//            System.out.println("***************** Automation Started *******************");
-//        }
-//
-//        @AfterTest
-//        public void tearDownAutomation() {
-//            System.out.println("***************** Automation End *******************");
-//            driver.close();
-//        }
-//
-//        @BeforeTest
-//        public static void setUpChromeBrowser() {
-//            String chromeDriverPath = "../Day_41_Automation/BrowserDriver/Windows/chromedriver.exe";
-//            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-//            driver = new ChromeDriver();
-//
-//            //  navigate to target.com
-//            driver.get(url);
-//            driver.manage().window().maximize();
-//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-//            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-//            driver.manage().deleteAllCookies();
-//        }
+
+    public static WebDriver driver;
+    String browserName = "chrome";
+    static String url = "https://www.target.com/";
 
 
+    @BeforeTest
+    public void setUpAutomation() {
+
+        System.out.println("***************** Automation Started *******************");
+    }
+
+    @AfterTest
+    public void tearDownAutomation() {
+        System.out.println("***************** Automation End *******************");
+        driver.close();
+    }
+
+    @BeforeTest
+    public static void setUpChromeBrowser() {
+        String chromeDriverPath = "../Day_41_Automation/BrowserDriver/Windows/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        driver = new ChromeDriver();
+
+        //  navigate to target.com
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().deleteAllCookies();
+    }
+
+    @Test
+    public void navigateToGoogle() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.navigate().to("https://www.google.com");
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).sendKeys("Dhaka City College");
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).click();
+        Thread.sleep(3000);
+        driver.navigate().back();
+        Thread.sleep(2000);
+        driver.navigate().forward();
+        Thread.sleep(2000);
+        driver.navigate().refresh();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).sendKeys("Dhaka University");
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).click();
+        Thread.sleep(2000);
+    }
 }
